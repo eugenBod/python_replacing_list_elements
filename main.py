6f201a3dd36add95312b9107c9fe4ce52d79dd8a
eugenBod <eugenybod@gmail.com>
2025-03-25 12:43:22 +0300

    Python replacing list elements (ver. 1.1)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def replace_list_element(list_elements, index, text):
+    list_elements[index] = text
+    return list_elements
+
+
 book_titles = ["Гарри Поттер и Тайная комната", "Гарри Поттер и Тайная комната",
                "Гарри Поттер и узник Азкабана", "Гарри Поттер и Кубок огня",
                "Гарри Поттер и Принц-полукровка"]
-
-book_titles[0] = "Гарри Поттер и философский камень"
-book_titles[-1] = "Гарри Поттер и Орден Феникса"
+replace_list_element(book_titles, 0, "Гарри Поттер и Философский камень")
+replace_list_element(book_titles, -1, "Гарри Поттер и Орден Феникса")
 print(book_titles)
